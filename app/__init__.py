0e31844c2a24cb78e13b361e6e3e46a6ee5f1452
nacer aoui <nasseraouissi0@gmail.com>
2025-04-15 14:14:20 +0100

    e2

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = 'supersecretkey'  # ✅ THEN set secret key
 
     from .routes import main
     app.register_blueprint(main)
